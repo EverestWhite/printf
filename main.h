2b7e5b8837b07aa838b69157caba08c1128744dd
EverestWhite <132360161+EverestWhite@users.noreply.github.com>
2023-07-25 18:02:32 +0100

    main.h

--- main.h
+++ main.h
@@ -1,6 +1,33 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
 int _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
